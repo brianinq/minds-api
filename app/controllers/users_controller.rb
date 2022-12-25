df87706e5d08b2b6b04e4ccdee74a70141c710f1
brian <kipkoechbrian0@gmail.com>
2022-12-25 17:01:13 +0300

    update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,20 @@ class UsersController < ApplicationController
             render json: {error: "Failed to create user"}, status: :unprocessable_entity
         end 
     end
+    def update
+        user = User.find(params[:id])
+        user.update!(user_params)
+        render json: user
+    end
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
 
 
     private
     def user_params
-        params.permit(:name, :username, :password, :password_confirmation, :email, :age, :gender, :avatar )
+        params.permit(:name, :username, :password, :password_confirmation, :email, :age, :gender, :avatar, :is_counselor )
     end
     def ivalid_record(e)
         render json: {errors: e.record.errors.full_messages}, status: 422
