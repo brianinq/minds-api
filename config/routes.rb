d21920edccceed45d4d3aabe7e5bb722afd63065
brian <kipkoechbrian0@gmail.com>
2022-12-25 16:58:41 +0300

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:create, :update]
+  resources :users, only: [:create, :update, :show] do
+    resources :appointments, only:[:index, :create, :show, :update, :destroy]
+    post '/new_from_session', to: 'appointments#new_from_session'
+  end
+  resources :appointments, only:[:index, :update, :destroy]
+  resources :sessions, only: [:index, :create, :update, :show, :destroy]
+  resources :reviews, only: [:index, :create, :update, :show, :destroy]
+  resources :counselor_profiles, only: [:index, :create, :show, :update]
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
 end
