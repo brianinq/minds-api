e834c0eb5278698393aa94a3cb89f848ad7d4b12
brian <kipkoechbrian0@gmail.com>
2023-01-15 15:12:55 +0300

    small typo fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :email, :avatar, :gender, :age, :is_counselor, :is_admin
+  attributes :id, :name, :username, :email, :avatar, :gender, :age, :is_counselor, :is_admin, :counselor_profile
+  
+  def counselor_profile
+    if object.is_counselor
+      profile = CounselorProfile.where(user_id: object.id)
+      return profile
+    end
+  end
 end
